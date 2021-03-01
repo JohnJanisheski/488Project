3858263311c965591d92f1cfc54c095c77d9d592
Sarah Derr <scd5325@psu.edu>
2021-03-01 17:58:22 -0500

    added capability to add items and only show items if you are logged in to the website. everything can be linked back to the firebase project itself.

--- Mini Market/auth.js
+++ Mini Market/auth.js
@@ -5,10 +5,17 @@
 <!-- auth state changes -->
 auth.onAuthStateChanged(user => {
     if (user){
-        console.log('user logged in: ', user)
+        console.log('user logged in: ', user);
+        db.collection('items').get().then(snapshot => {
+            snapshot.docs.forEach(doc =>{
+                console.log(doc.data());
+                renderList(doc);
+            })
+        });
     }
     else{
         console.log('user logged out');
+        itemlist.innerHTML = '<h5 class="center-align">Login to view items!</h5>'
     }
 })
 
@@ -47,3 +54,46 @@ login.addEventListener('submit', (e) =>{
         login.reset();
     });
 });
+
+const addItemsForm = document.querySelector('#add-item-form');
+addItemsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('items').add({
+        title: addItemsForm['title'].value,
+        description: addItemsForm['description'].value,
+        type: addItemsForm['type'].value
+
+    }).then(() => {
+        const modal = document.querySelector('#modal-add');
+        M.Modal.getInstance(modal).close();
+        addItemsForm.reset();
+    });
+});
+
+
+<!-- www.youtube.com/watch?v=zpQle4SBRfg&list=PL4cUxeGkcC9itfjle0ji1xOZ2cjRGY_WB&index=4&t=24s -->
+const itemlist = document.querySelector('.itemslist');
+
+function renderList(doc){
+    let li = document.createElement('li');
+    let title = document.createElement('span');
+    let desc = document.createElement('span');
+    let type = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    title.textContent = doc.data().title;
+    desc.textContent = doc.data().description;
+    type.textContent = doc.data().type;
+
+    li.appendChild(title);
+    li.appendChild(desc);
+    li.appendChild(type);
+    itemlist.appendChild(li);
+}
+
+
+
+
+
+
