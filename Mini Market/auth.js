df26294e12e3f055ef0b8cfd5c2b7b0723595437
Sarah Derr <scd5325@psu.edu>
2021-03-08 12:29:45 -0500

    updated the js files so no more pop-out modals! yay! i'm still trying to update the user and item connection but hopefully this works for everyone. I also added conditional menu links that show depending on if you're logged in or not

--- Mini Market/auth.js
+++ Mini Market/auth.js
@@ -2,54 +2,42 @@
 <!-- "Firebase auth tutorial creating new users -->
 <!-- www.youtube.com/watch?v=wkdCpktUfGg&list=PL4cUxeGkcC9jUPIes_B8vRjn1_GaplOPQ&index=5  -->
 
+
 <!-- auth state changes -->
 auth.onAuthStateChanged(user => {
     if (user){
         console.log('user logged in: ', user);
-        db.collection('items').get().then(snapshot => {
+        db.collection('items').onSnapshot(snapshot => {
             snapshot.docs.forEach(doc =>{
                 console.log(doc.data());
                 renderList(doc);
             })
         });
+        setupUI(user);
 
-        itemlist.innerHTML = '';
+
     }
     else{
         console.log('user logged out');
+        setupUI();
         itemlist.innerHTML = '<h5 class="center-align">Login to view items!</h5>'
     }
 });
 
-const account = document.querySelector('#account-details');
-const checkUser = (user) => {
-    if (user) {
-        const html = '<div>Logged in as ${user.email}</div>';
-        account.innerHTML = html;
-    }
-    else{
-        account.innerHTML = '';
-    }
-};
 
-// signup
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
+<!-- login current users -->
+const login = document.querySelector('#login-form');
+const loginContainer = document.querySelector('#login');
+
+login.addEventListener('submit', (e) =>{
     e.preventDefault();
-    const userEmail = signupForm['signup-email'].value;
-    const userPassword = signupForm['signup-password'].value;
+    const userEmail = login['login-email'].value;
+    const userPassword = login['login-password'].value;
 
-    auth.createUserWithEmailAndPassword(userEmail, userPassword).then(cred => {
-        return db.collection('accounts').doc(cred.user.id).set({
-            bio: signupForm['signup-bio'].value,
-            name: signupForm['signup-name'].value,
-            campus: signupForm['signup-campus'].value,
-        })
+    auth.signInWithEmailAndPassword(userEmail, userPassword).then(cred => {
+        login.reset();
+        loginContainer.style.display="none";
 
-    }).then(() => {
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-        signupForm.reset();
     });
 });
 
@@ -60,55 +48,45 @@ logout.addEventListener('click', (e) => {
     auth.signOut();
 });
 
-<!-- login current users -->
-const login = document.querySelector('#login-form')
-login.addEventListener('submit', (e) =>{
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+const signupContainer = document.querySelector('#signup');
+
+signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const userEmail = login['login-email'].value;
-    const userPassword = login['login-password'].value;
+    const userEmail = signupForm['signup-email'].value;
+    const userPassword = signupForm['signup-password'].value;
 
-    auth.signInWithEmailAndPassword(userEmail, userPassword).then(cred => {
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        login.reset();
-    });
-});
-
-<!-- add items -->
-const addItemsForm = document.querySelector('#add-item-form');
-addItemsForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    db.collection('items').add({
-        title: addItemsForm['title'].value,
-        description: addItemsForm['description'].value,
-        type: addItemsForm['type'].value
+    auth.createUserWithEmailAndPassword(userEmail, userPassword).then(cred => {
+        return db.collection('accounts').doc(cred.user.uid).set({
+            bio: signupForm['signup-bio'].value,
+            name: signupForm['signup-name'].value,
+            campus: signupForm['signup-campus'].value,
+        })
 
     }).then(() => {
-        const modal = document.querySelector('#modal-add');
-        M.Modal.getInstance(modal).close();
-        addItemsForm.reset();
+        signupForm.reset();
+        signupContainer.style.display="none";
     });
 });
 
 
-<!-- create profile -->
-const accountForm = document.querySelector('#account-details-form');
-accountForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+<!-- conditional menu links -->
+document.getElementById("log-in-button").addEventListener("click", () => {
+    document.getElementById("additem").style.display="none";
+    document.getElementById("login").style.display="block";
+    document.getElementById("signup").style.display="none";
+    document.getElementById("itemsadded").style.display="none";
 
-    db.collection('users').add({
-        name: accountForm['name'].value,
-        email: accountForm['email'].value,
-        campus: accountForm['campus'].value,
-        bio: accountForm['bio'].value
+}, false);
 
-    }).then(() => {
-        const modal = document.querySelector('#modal-add');
-        M.Modal.getInstance(modal).close();
-        accountForm.reset();
-    });
-});
+document.getElementById("sign-up-button").addEventListener("click", () => {
+    document.getElementById("additem").style.display="none";
+    document.getElementById("login").style.display="none";
+    document.getElementById("signup").style.display="block";
+    document.getElementById("itemsadded").style.display="none";
+}, false);
 
 
 <!-- www.youtube.com/watch?v=zpQle4SBRfg&list=PL4cUxeGkcC9itfjle0ji1xOZ2cjRGY_WB&index=4&t=24s -->
@@ -120,19 +98,38 @@ function renderList(doc){
     let title = document.createElement('span');
     let desc = document.createElement('span');
     let type = document.createElement('span');
+    let del = document.createElement('del');
 
     li.setAttribute('data-id', doc.id);
     title.textContent = doc.data().title;
     desc.textContent = doc.data().description;
     type.textContent = doc.data().type;
+    del.textContent = 'X';
 
     li.appendChild(title);
     li.appendChild(desc);
     li.appendChild(type);
+    li.appendChild(del);
     itemlist.appendChild(li);
+
+    del.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('items').doc(id).delete();
+    });
 }
 
-
+const addItemsForm = document.querySelector('#add-item-form');
+addItemsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('items').add({
+        title: addItemsForm['title'].value,
+        description: addItemsForm['description'].value,
+        type: addItemsForm['type'].value
+    }).then(() => {
+        addItemsForm.reset();
+    })
+});
 
 
 
