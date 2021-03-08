b5149900acc9b16dd9ae7ac3960751fd555cfaa1
John <JohnJanisheski99@gmail.com>
2021-03-08 16:48:19 -0500

    refreshingj and updating from add item to firestore

--- Mini Market/scripts/auth.js
+++ Mini Market/scripts/auth.js
@@ -5,22 +5,52 @@
 <!-- auth state changes -->
 auth.onAuthStateChanged(user => {
     if (user){
-        console.log('user logged in: ', user)
+        console.log('user logged in: ', user);
+        db.collection('items').get().then(snapshot => {
+            snapshot.docs.forEach(doc =>{
+                console.log(doc.data());
+                renderList(doc);
+            })
+        });
+
+        itemlist.innerHTML = '';
     }
     else{
         console.log('user logged out');
+        itemlist.innerHTML = '<h5 class="center-align">Login to view items!</h5>'
     }
-})
+});
 
+const account = document.querySelector('#account-details');
+const checkUser = (user) => {
+    if (user) {
+        const html = '<div>Logged in as ${user.email}</div>';
+        account.innerHTML = html;
+    }
+    else{
+        account.innerHTML = '';
+    }
+};
 
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    // Get Users Email and Potential Password
     const userEmail = signupForm['signup-email'].value;
     const userPassword = signupForm['signup-password'].value;
 
-    auth.createUserWithEmailAndPassword(userEmail, userPassword).then(credential => {
+    // Attempt to create new user
+    auth.createUserWithEmailAndPassword(userEmail, userPassword).then(cred => {
+        return db.collection('accounts').doc(cred.user.id).set({
+            bio: signupForm['signup-bio'].value,
+            firstName: signupForm['signup-first-name'].value,
+            lastName: signupForm['signup-last-name'].value,
+            campus: signupForm['signup-campus'].value,
+        })
+
+    }).then(() => {
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
@@ -47,3 +77,67 @@ login.addEventListener('submit', (e) =>{
         login.reset();
     });
 });
+
+<!-- add items -->
+const addItemsForm = document.querySelector('#add-item-form');
+addItemsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('items').add({
+        title: addItemsForm['title'].value,
+        description: addItemsForm['description'].value,
+        type: addItemsForm['type'].value
+
+    }).then(() => {
+        const modal = document.querySelector('#modal-add');
+        M.Modal.getInstance(modal).close();
+        addItemsForm.reset();
+    });
+});
+
+
+<!-- create profile -->
+const accountForm = document.querySelector('#account-details-form');
+accountForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('users').add({
+        name: accountForm['name'].value,
+        email: accountForm['email'].value,
+        campus: accountForm['campus'].value,
+        bio: accountForm['bio'].value
+
+    }).then(() => {
+        const modal = document.querySelector('#modal-add');
+        M.Modal.getInstance(modal).close();
+        accountForm.reset();
+    });
+});
+
+
+<!-- www.youtube.com/watch?v=zpQle4SBRfg&list=PL4cUxeGkcC9itfjle0ji1xOZ2cjRGY_WB&index=4&t=24s -->
+<!-- show items list -->
+const itemlist = document.querySelector('.itemslist');
+
+function renderList(doc){
+    let li = document.createElement('li');
+    let title = document.createElement('span');
+    let desc = document.createElement('span');
+    let type = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    title.textContent = doc.data().title;
+    desc.textContent = doc.data().description;
+    type.textContent = doc.data().type;
+
+    li.appendChild(title);
+    li.appendChild(desc);
+    li.appendChild(type);
+    itemlist.appendChild(li);
+}
+
+
+
+
+
+
--- public/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function (){
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-
-    var items = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(items);
-});
