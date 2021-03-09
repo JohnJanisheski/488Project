f021e66f344aaffad7a3775f547401c63b1a696d
John <JohnJanisheski99@gmail.com>
2021-03-08 19:57:12 -0500

    Start of Messaging System

---
+++ Mini Market/scripts/messaging.js
@@ -0,0 +1,8 @@
+const message = firebase.messaging();
+message.requestPermission()
+.then(function() {
+    console.log('Have permission'); //User Can Allow or Deny messages
+})
+.catch(function(err) {
+    console.log('Error Occurred.')
+})
