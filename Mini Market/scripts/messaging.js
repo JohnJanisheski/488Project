3b7662258595548dc154297ddd2484c9e54941f7
tzp280 <73612285+tzp280@users.noreply.github.com>
2021-03-08 20:03:25 -0500

    Update messaging.js

--- Mini Market/scripts/messaging.js
+++ Mini Market/scripts/messaging.js
@@ -1,2 +1,14 @@
+const message = firebase.messaging();
 
+message.requestPermission()
+    .then(function() {
+        console.log('Have permission'); //User Can Allow or Deny messages
+        return message.getToken();
+    })
+    .then(function(token) {
+        console.log(token);
+    })
+    .catch(function(err) {
+        console.log('Error Occured.')
+    })
 
