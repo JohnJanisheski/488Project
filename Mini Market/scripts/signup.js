5b8f29eb164343d96b5a27de6fd34235da7da0bb
Sarah Derr <scd5325@psu.edu>
2021-04-15 03:02:12 -0400

    incorporated adding a photo to your profile. all new profiles will now have a default photo that the user can change afterwards. if you already have a profile, i suggest adding one just in case the system yells at you or something. similar to the items photo-upload, it's through the firebase storage under "icons" and can be accessible through the "downloadURL"

--- Mini Market/scripts/signup.js
+++ Mini Market/scripts/signup.js
@@ -18,14 +18,14 @@ signupForm.addEventListener('submit', (e) =>{
     // Attempt to create user with proposed email, password, Bio, First and last name and their desired campus
     auth.createUserWithEmailAndPassword(userEmail, userPassword).then(cred => {
         return db.collection('accounts').doc(cred.user.uid).set({
-            firstname: signupForm['firstname'].value,
-            lastname: signupForm['lastname'].value,
+            downloadURL: "https://firebasestorage.googleapis.com/v0/b/mini-market-aa3c4.appspot.com/o/icons%2Fprofileimage.jpg?alt=media&token=6830f44b-9b37-46f8-a72e-a501428335dc",
+            firstname: signupForm['first-name'].value,
+            lastname: signupForm['last-name'].value,
             campus: signupForm['campus'].value,
             phone: signupForm['phone'].value,
             email: signupForm['email'].value + "@psu.edu",
         })
     }).then(() => {
-        alert("Your account has been created!")
         console.log("User Created")
         signupForm.reset();
         signupContainer.style.display="none";
--- Mini Market/scripts/userInfo.js
+++ Mini Market/scripts/userInfo.js
@@ -2,12 +2,13 @@
 auth.onAuthStateChanged(user => {
     db.collection('accounts').doc(user.uid).get().then(doc => {
         const info = doc.data();
-        document.getElementsByName('email')[0].value=user.email;
+        document.getElementById('showImage').src = info.downloadURL;
+        document.getElementsByName('email')[0].value = user.email;
         document.getElementsByName('account')[0].value = info.firstname + " " + info.lastname;
-        document.getElementsByName('campus')[0].value=info.campus;
-        document.getElementsByName('phoneNumber')[0].value=info.phone;
-        document.getElementsByName('firstName')[0].value=info.firstname;
-        document.getElementsByName('lastName')[0].value=info.lastname;
+        document.getElementsByName('campus')[0].value = info.campus;
+        document.getElementsByName('phoneNumber')[0].value = info.phone;
+        document.getElementsByName('firstName')[0].value = info.firstname;
+        document.getElementsByName('lastName')[0].value = info.lastname;
 
     });
 });
