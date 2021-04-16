89d5180b153a3fac7c5213b279e7b60833eeee83
tzp280 <73612285+tzp280@users.noreply.github.com>
2021-04-16 04:22:20 -0400

    Update box-load.js
    box-load will now only show boxes of user's friends.

--- Mini Market/scripts/box-load.js
+++ Mini Market/scripts/box-load.js
@@ -1,34 +1,83 @@
 var htmlElements = "";
 var boxes = document.getElementById("friendBoxes");
 //users3 = ["John", "Tyler", "Yuhui", "Laura"] //This is the user's list of friends will be in the database not hardcoded
-users3 =[]
-db.collection("accounts").get().then((querySnapshot) => {//Adds all of emails in the database to a list
-    querySnapshot.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
-        console.log(doc.data().firstname);
-        users3.push(doc.data().firstname);
-    });
+users3 =[] //firstnames
+userEmails = [];
+currEmails = [];
+currAccounts = [];
+count = [];
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        //console.log(user.uid); //Returns THE CURRENT USER
+        userId = user.uid;
+        userEmail = user.email;
 
-    console.log(users3);
-    for (var i = 0; i < users3.length; i++) {
-        htmlElements += '<div class="userTitle2 " onclick= changeTitle('+i+')>\n' +
-            '                    <img src="./img/thinkinJava1.jpg" style="width: 45px; height: 45px; border-radius: 22.5px;">\n' +
-            '                    <span id = "buddy' + i + '" style="margin-left: 15px;font-size: 32;font-weight: 500;"><script>console.log(i);</script></span></div>';
+        db.collection("accounts").get().then((querySnapshot) => {//Adds all of emails in the database to a list
+            querySnapshot.forEach((doc) => {
+                //console.log(doc.id, " => ", doc.data());
+                //console.log(doc.data().firstname);
+                //users3.push(doc.data().firstname);
+                userEmails.push(doc.data().email);
+            });
+
+
+
+            for(x = 0; x<userEmails.length;x++) {
+                    db.collection("friends").doc(userId).collection(userEmails[x]).get().then((querySnapshot) => {//Adds all of emails in the database to a list
+                        querySnapshot.forEach((doc) => {
+                            console.log(doc.data().email);
+                            console.log(doc.data().firstname);
+                            users3.push(doc.data().firstname + " " + doc.data().lastname);
+                            //console.log(x);
+                            //console.log(count);
+
+                            for (var i = 0; i < users3.length; i++) {
+                                if(!count.includes(i)) {
+                                htmlElements += '<div class="userTitle2 " onclick= changeTitle(' + i + ')>\n' +
+                                    '                    <img src="./img/thinkinJava1.jpg" style="width: 45px; height: 45px; border-radius: 22.5px;">\n' +
+                                    '                    <span id = "buddy' + i + '" style="margin-left: 15px;font-size: 32;font-weight: 500;"></span></div>';
+                                console.log(count);
+
+                                    count.push(i);
+
+                                }
+
+                            }
+
+                            boxes.innerHTML = htmlElements;
+                            boxGroup = [];
+                            for (y = 0; y < users3.length; y++) {
+                                box = document.getElementById("buddy" + y);
+                                //console.log(y);
+                                //console.log(users3[y]);
+                                box.innerText = users3[y];
+                                boxGroup.push(box);
+                                console.log(boxGroup);
+                            }
+
+
+                        });
+
+
+
+                    });
+                }
+
+
+
+        });
+
 
 
     }
-
-    boxes.innerHTML = htmlElements;
-    boxGroup = [];
-    for (x = 0; x<users3.length; x++){
-        box = document.getElementById("buddy" + x);
-        box.innerText = users3[x];
-        boxGroup.push(box);
-
-    }
-
 
 });
+
+
+
+
+
+
 function loadUser() {
     eMail = firebase.auth().currentUser.email;
     console.log(eMail);
@@ -47,14 +96,14 @@ function loadUser() {
             console.log(eMails[i]);
             if (eMail === eMails[i]) {
                 //console.log(accounts[i]);
-                document.getElementById("user").innerText = accounts[i];
+                //document.getElementById("user").innerText = accounts[i];
             }
 
         }
     });
 }
 
-function changeTitle(y){
+function changeTitle(y) {
     accounts2 = [];
     eMails2 = [];
     box2 = document.getElementById("buddy" + y);
@@ -71,22 +120,33 @@ function changeTitle(y){
             accounts2.push(doc.data().firstname);
         });
 
-        for(x = 0; x<accounts2.length;x++) {
-            if (person === accounts2[x]) {
-                personEmail = eMails2[x];
 
-            }
-        }
-        recipient(personEmail);
     });
 
     loadMessages(person);
 
 
-
-
-
-
 }
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        //console.log(user);
+        currUser = user.email;
+        db.collection("accounts").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                //console.log(doc.id, " => ", doc.data());
+                //console.log(doc.data().email);
+                currEmails.push(doc.data().email);
+                currAccounts.push(doc.data().firstname + " " +  doc.data().lastname);
+            });
+            for (i = 0; i < currEmails.length; i++) {
+                if (currUser === currEmails[i]) {
+                    document.getElementById("user").innerText = currAccounts[i];
+                }
 
+            }
+
+
+        });
+    }
+});
