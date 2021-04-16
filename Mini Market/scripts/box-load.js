6769e5188783ad349ae1929523ec2eedda61481a
tzp280 <73612285+tzp280@users.noreply.github.com>
2021-04-16 06:59:44 -0400

    Update box-load.js

--- Mini Market/scripts/box-load.js
+++ Mini Market/scripts/box-load.js
@@ -25,8 +25,8 @@ firebase.auth().onAuthStateChanged(function(user) {
             for(x = 0; x<userEmails.length;x++) {
                     db.collection("friends").doc(userId).collection(userEmails[x]).get().then((querySnapshot) => {//Adds all of emails in the database to a list
                         querySnapshot.forEach((doc) => {
-                            console.log(doc.data().email);
-                            console.log(doc.data().firstname);
+                            //console.log(doc.data().email);
+                            //console.log(doc.data().firstname);
                             users3.push(doc.data().firstname + " " + doc.data().lastname);
                             //console.log(x);
                             //console.log(count);
@@ -36,7 +36,7 @@ firebase.auth().onAuthStateChanged(function(user) {
                                 htmlElements += '<div class="userTitle2 " onclick= changeTitle(' + i + ')>\n' +
                                     '                    <img src="./img/thinkinJava1.jpg" style="width: 45px; height: 45px; border-radius: 22.5px;">\n' +
                                     '                    <span id = "buddy' + i + '" style="margin-left: 15px;font-size: 32;font-weight: 500;"></span></div>';
-                                console.log(count);
+                                //console.log(count);
 
                                     count.push(i);
 
@@ -52,7 +52,7 @@ firebase.auth().onAuthStateChanged(function(user) {
                                 //console.log(users3[y]);
                                 box.innerText = users3[y];
                                 boxGroup.push(box);
-                                console.log(boxGroup);
+                                //console.log(boxGroup);
                             }
 
 
@@ -80,7 +80,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 function loadUser() {
     eMail = firebase.auth().currentUser.email;
-    console.log(eMail);
+    //console.log(eMail);
     accounts = [];
     eMails = [];
 
@@ -90,13 +90,13 @@ function loadUser() {
             //console.log(doc.id, " => ", doc.data());
             //console.log(doc.data().email);
             eMails.push(doc.data().email);
-            accounts.push(doc.data().firstname);
+            accounts.push(doc.data().firstname + " " + doc.data().lastname);
         });
         for (i = 0; i < eMails.length; i++) {
-            console.log(eMails[i]);
+            //console.log(eMails[i]);
             if (eMail === eMails[i]) {
-                //console.log(accounts[i]);
-                //document.getElementById("user").innerText = accounts[i];
+                console.log(accounts[i]);
+                document.getElementById("user").innerText = accounts[i];
             }
 
         }
@@ -107,7 +107,7 @@ function changeTitle(y) {
     accounts2 = [];
     eMails2 = [];
     box2 = document.getElementById("buddy" + y);
-    //console.log(box2.innerText);
+    console.log(box2.innerText);
     document.getElementById("uT").innerText = document.getElementById("buddy" + y).innerText;
     //console.log(firebase.auth().currentUser.email);
     person = box2.innerText;
@@ -117,12 +117,19 @@ function changeTitle(y) {
             //console.log(doc.id, " => ", doc.data());
             //console.log(doc.data().email);
             eMails2.push(doc.data().email);
-            accounts2.push(doc.data().firstname);
+            accounts2.push(doc.data().firstname + " " + doc.data().lastname);
         });
+        for(x = 0; x<accounts2.length;x++) {
+            if (person === accounts2[x]) {
+                console.log(eMails2[x]);
+                personEmail = eMails2[x];
 
+            }
+        }
 
+        recipient(personEmail);
     });
-
+    htmlElements2 = '';
     loadMessages(person);
 
 
