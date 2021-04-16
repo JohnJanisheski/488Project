6130d8064bca49804f574822c213a50abd02e49f
tzp280 <73612285+tzp280@users.noreply.github.com>
2021-04-16 04:23:56 -0400

    Update add.js
    AddFriend Functionality Finished - User can now add friends as long as they have accounts.

--- Mini Market/scripts/add.js
+++ Mini Market/scripts/add.js
@@ -1,5 +1,7 @@
 function addFriend(){
     users = [];
+    firsts =[];
+    lasts = [];
     friend = prompt("Enter the email of the friend you would like to add");
     console.log(friend);
     if (friend == null || friend == "") {
@@ -14,15 +16,33 @@ function addFriend(){
         db.collection("accounts").get().then((querySnapshot) => {//Adds all of emails in the database to a list
             querySnapshot.forEach((doc) => {
                 console.log(doc.id, " => ", doc.data());
-                console.log(doc.data().firstname);
-                users.push(doc.data().firstname);
+                //console.log(doc.data().firstname);
+                users.push(doc.data().email);
+                firsts.push(doc.data().firstname);
+                lasts.push(doc.data().lastname);
                 //console.log(users);
 
 
             });
             if (users.includes(friend)) {
                 alert("User Added");
-                users3.push(friend); //This would add to the user's list of friends
+                for (x = 0; x < users.length; x++) {
+                    if (friend === users[x]) {
+                        fName2 = firsts[x];
+                        lName2 = lasts[x];
+                        db.collection("friends").doc(userId).collection(friend).add({
+                            email: friend,
+                            firstname: fName2,
+                            lastname: lName2,
+                        })
+                            .then((docRef) => {
+                                location.reload();
+                            })
+                            .catch((error) => {
+                                console.error("Error adding document: ", error);
+                            });
+                    }
+                }
             }
 
             if(!users.includes(friend)){
